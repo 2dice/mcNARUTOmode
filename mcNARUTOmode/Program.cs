d59dc93d8c3ca4381a92a531e468000a9645909f
2dice <rjhxx419@gmail.com>
2023-12-27 09:41:30 +0900

    マイクラと接続テスト

--- mcNARUTOmode/Program.cs
+++ mcNARUTOmode/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using System;
+using MinecraftConnection;
+
+internal class Program
 {
+    static string address = "127.0.0.1";
+    static ushort port = 25575;
+    static string pass = "minecraft";
+    static MinecraftCommands command = new MinecraftCommands(address, port, pass);
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // TODO:インスタンスの作成
+
+        // メインループ処理
+        while (true)
+        {
+            command.Wait(300);
+            Console.WriteLine("Hello, World!");
+        }
     }
 }
