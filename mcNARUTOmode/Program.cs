9fc3e93ffc5d7bae6f5204049b79903124806978
2dice <rjhxx419@gmail.com>
2023-12-30 16:01:27 +0900

    リファクタリング：命名規則に則り変数名/インスタンス名/関数名等を修正

--- mcNARUTOmode/Program.cs
+++ mcNARUTOmode/Program.cs
@@ -5,15 +5,15 @@ using MinecraftConnection;
 internal class Setup
 {
     //Minecraft Setup
-    static readonly string address = "127.0.0.1";
-    static readonly ushort port = 25575;
-    static readonly string pass = "minecraft";
-    internal static readonly MinecraftCommands command = new(address, port, pass);
+    static readonly string _address = "127.0.0.1";
+    static readonly ushort _port = 25575;
+    static readonly string _pass = "minecraft";
+    internal static readonly MinecraftCommands Command = new(_address, _port, _pass);
     //Player Setup
     internal static readonly string PlayerName = "2dice_K";
-    internal static readonly PlayerStatus player = new();
+    internal static readonly PlayerStatus Player = new();
     //Ninja Setup
-    internal static readonly SandNinja sandNinja = new();
+    internal static readonly SandNinja SandNinja = new();
 }
 
 internal class Program
@@ -23,9 +23,9 @@ internal class Program
         // メインループ処理
         while (true)
         {
-            Setup.command.Wait(30);
-            Setup.player.UpdateStatus();
-            Setup.sandNinja.PreventFallDamage();
+            Setup.Command.Wait(30);
+            Setup.Player.UpdateStatus();
+            Setup.SandNinja.PreventFallDamage();
             //Console.WriteLine(Setup.player.FallDistance);
         }
     }
@@ -48,21 +48,21 @@ internal class PlayerStatus
     {
         Console.WriteLine("PlayerStatusInit!");
         //スコアボードの作成(sand)
-        Setup.command.SendCommand("scoreboard objectives add sandRightClicked minecraft.used:minecraft.sand \"sand right clicked\"");
-        Setup.command.SendCommand("scoreboard players set @a sandRightClicked 0");
+        Setup.Command.SendCommand("scoreboard objectives add sandRightClicked minecraft.used:minecraft.sand \"sand right clicked\"");
+        Setup.Command.SendCommand("scoreboard players set @a sandRightClicked 0");
     }
 
     //update関数(座標等の取得
     internal void UpdateStatus()
     {
-        UpdatePlayerPosition();
-        UpdatePlayerMotion();
-        UpdatePlayerFallDistance();
-        UpdatePlayerSelectedItem();
-        UpdatePlayerSelectedItemUsed();
-        UpdateNearestEnemyPosition();
+        updatePlayerPosition();
+        updatePlayerMotion();
+        updatePlayerFallDistance();
+        updatePlayerSelectedItem();
+        updatePlayerSelectedItemUsed();
+        updateNearestEnemyPosition();
     }
-    private void UpdatePlayerPosition()
+    private void updatePlayerPosition()
     {
         //前回値の保持
         LastPosition = LatestPosition;
@@ -70,13 +70,13 @@ internal class PlayerStatus
         try
         {
             //ここで稀に例外が発生(型が一致しない)
-            Position pos = Setup.command.GetPlayerData(Setup.PlayerName).Position;
+            Position _pos = Setup.Command.GetPlayerData(Setup.PlayerName).Position;
             // 整数座標に変換
-            double x = Math.Floor(pos.X);
-            double y = Math.Floor(pos.Y);
-            double z = Math.Floor(pos.Z);
+            double _x = Math.Floor(_pos.X);
+            double _y = Math.Floor(_pos.Y);
+            double _z = Math.Floor(_pos.Z);
             // 更新したら前のインスタンスはGCで開放される
-            LatestPosition = new Position(x, y, z);
+            LatestPosition = new Position(_x, _y, _z);
         }
         catch (Exception e)
         {
@@ -84,20 +84,20 @@ internal class PlayerStatus
         }
     }
 
-    private void UpdatePlayerMotion()
+    private void updatePlayerMotion()
     {
         //TODO:Yデータだけでよければコマンドを2つ減らせる(xyz個別にサーバーに問い合わせているので
-        var mot = Setup.command.GetPlayerData(Setup.PlayerName).Motion;
+        Motion _mot = Setup.Command.GetPlayerData(Setup.PlayerName).Motion;
         // 更新したら前のインスタンスはGCで開放される
-        Motion = new Motion(mot.X, mot.Y, mot.Z);
+        Motion = new Motion(_mot.X, _mot.Y, _mot.Z);
     }
 
-    private void UpdatePlayerFallDistance()
+    private void updatePlayerFallDistance()
     {
         try
         {
             //ここで稀に例外が発生(型が一致しない)
-            FallDistance = Setup.command.GetPlayerData(Setup.PlayerName).FallDistance;
+            FallDistance = Setup.Command.GetPlayerData(Setup.PlayerName).FallDistance;
         }
         catch (Exception e)
         {
@@ -105,34 +105,34 @@ internal class PlayerStatus
         }
     }
 
-    private void UpdatePlayerSelectedItem()
+    private void updatePlayerSelectedItem()
     {
-        var result = Setup.command.SendCommand("data get entity " + Setup.PlayerName + " SelectedItem");
+        var _result = Setup.Command.SendCommand($"data get entity {Setup.PlayerName} SelectedItem");
         //正規表現で""に囲まれた文字列の抽出
-        var pickItem = new Regex("\"(.+?)\"");
-        var match = pickItem.Match(result);
-        SelectedItemName = match.Groups[1].Value;
+        var _pickItem = new Regex("\"(.+?)\"");
+        var _match = _pickItem.Match(_result);
+        SelectedItemName = _match.Groups[1].Value;
     }
 
-    private void UpdatePlayerSelectedItemUsed()
+    private void updatePlayerSelectedItemUsed()
     {
         //2dice_K has 0 [sand right clicked]
-        string result = "";
+        string _result = "";
         switch (SelectedItemName)
         {
             case "minecraft:sand":
-                result = Setup.command.SendCommand("scoreboard players get @p sandRightClicked");
+                _result = Setup.Command.SendCommand("scoreboard players get @p sandRightClicked");
                 break;
             default:
                 break;
         }
         //正規表現でスペースに囲まれた1桁の数字を抽出
-        Regex regex = new Regex(@"\s(\d)\s");
-        Match match = regex.Match(result);
-        if (match.Groups[1].Value == "1")
+        Regex _regex = new Regex(@"\s(\d)\s");
+        Match _match = _regex.Match(_result);
+        if (_match.Groups[1].Value == "1")
         {
             SelectedItemIsUsed = true;
-            Setup.command.SendCommand("scoreboard players set @a sandRightClicked 0");
+            Setup.Command.SendCommand("scoreboard players set @a sandRightClicked 0");
         }
         else
         {
@@ -141,52 +141,52 @@ internal class PlayerStatus
 
     }
 
-    private void UpdateNearestEnemyPosition()
+    private void updateNearestEnemyPosition()
     {
         //Spider has the following entity data: [-11.455525245656695d, 119.0d, 6.752002335797372d]
-        string result = Setup.command.SendCommand("execute as @e[type=!player,type=!item,sort=nearest,limit=1] run data get entity @s Pos");
+        string _result = Setup.Command.SendCommand("execute as @e[type=!player,type=!item,sort=nearest,limit=1] run data get entity @s Pos");
         // 正規表現で数値を抜き出す([]内の文字列を抽出)
-        var regex = new Regex(@"\[(.+?)\]");
-        Match match = regex.Match(result);
-        string[] parts = match.Groups[1].Value.Split(',');
-        double enemy_pos_x = Math.Floor(Double.Parse(parts[0].TrimEnd('d')));
-        double enemy_pos_y = Math.Floor(Double.Parse(parts[1].TrimEnd('d')));
-        double enemy_pos_z = Math.Floor(Double.Parse(parts[2].TrimEnd('d')));
+        var _regex = new Regex(@"\[(.+?)\]");
+        Match _match = _regex.Match(_result);
+        string[] _parts = _match.Groups[1].Value.Split(',');
+        double _enemy_pos_x = Math.Floor(Double.Parse(_parts[0].TrimEnd('d')));
+        double _enemy_pos_y = Math.Floor(Double.Parse(_parts[1].TrimEnd('d')));
+        double _enemy_pos_z = Math.Floor(Double.Parse(_parts[2].TrimEnd('d')));
         // 更新したら前のインスタンスはGCで開放される
-        NearestEnemyPosition = new Position(enemy_pos_x, enemy_pos_y, enemy_pos_z);
+        NearestEnemyPosition = new Position(_enemy_pos_x, _enemy_pos_y, _enemy_pos_z);
     }
 }
 
 internal class Ninja
 {
-    protected string UseBlockName { get; set; } = "barrier";
+    protected string useBlockName { get; set; } = "barrier";
     //コンストラクタ
     internal Ninja()
     {
-        UseBlockName = "barrier";
+        useBlockName = "barrier";
     }
 
     //設置位置保持用変数
-    protected Position PreventBlockPosition = new(0, 0, 0);
-    protected bool PreventBlockSetFlag = false;
+    protected Position preventBlockPosition = new(0, 0, 0);
+    protected bool preventBlockSetFlag = false;
     internal virtual void PreventFallDamage()
     {
-        if (Setup.player.FallDistance >= 2 && PreventBlockSetFlag == false)
+        if (Setup.Player.FallDistance >= 2 && preventBlockSetFlag == false)
         {
             // 更新したら前のインスタンスはGCで開放される
-            PreventBlockPosition = new Position(Setup.player.LatestPosition.X, Setup.player.LatestPosition.Y - 2, Setup.player.LatestPosition.Z);
+            preventBlockPosition = new Position(Setup.Player.LatestPosition.X, Setup.Player.LatestPosition.Y - 2, Setup.Player.LatestPosition.Z);
             //配置場所がairかどうか確認
-            string result = Setup.command.SendCommand($"execute if block {PreventBlockPosition.X} {PreventBlockPosition.Y} {PreventBlockPosition.Z} air");
-            if (result == "Test passed")
+            string _result = Setup.Command.SendCommand($"execute if block {preventBlockPosition.X} {preventBlockPosition.Y} {preventBlockPosition.Z} air");
+            if (_result == "Test passed")
             {
-                Setup.command.SendCommand($"fill {PreventBlockPosition.X - 1} {PreventBlockPosition.Y} {PreventBlockPosition.Z - 1} {PreventBlockPosition.X + 1} {PreventBlockPosition.Y + 1} {PreventBlockPosition.Z + 1} minecraft:{UseBlockName}");
-                PreventBlockSetFlag = true;
+                Setup.Command.SendCommand($"fill {preventBlockPosition.X - 1} {preventBlockPosition.Y} {preventBlockPosition.Z - 1} {preventBlockPosition.X + 1} {preventBlockPosition.Y + 1} {preventBlockPosition.Z + 1} minecraft:{useBlockName}");
+                preventBlockSetFlag = true;
             }
         }
-        else if (Setup.player.FallDistance == 0 && PreventBlockSetFlag == true)
+        else if (Setup.Player.FallDistance == 0 && preventBlockSetFlag == true)
         {
-            Setup.command.SendCommand($"fill {PreventBlockPosition.X - 1} {PreventBlockPosition.Y} {PreventBlockPosition.Z - 1} {PreventBlockPosition.X + 1} {PreventBlockPosition.Y + 1} {PreventBlockPosition.Z + 1} minecraft:air[] destroy");
-            PreventBlockSetFlag = false;
+            Setup.Command.SendCommand($"fill {preventBlockPosition.X - 1} {preventBlockPosition.Y} {preventBlockPosition.Z - 1} {preventBlockPosition.X + 1} {preventBlockPosition.Y + 1} {preventBlockPosition.Z + 1} minecraft:air[] destroy");
+            preventBlockSetFlag = false;
         }
     }
 }
@@ -194,27 +194,27 @@ internal class SandNinja : Ninja
 {
     internal SandNinja()
     {
-        UseBlockName = "sand";
+        useBlockName = "sand";
     }
     internal override void PreventFallDamage()
     {
-        if (Setup.player.FallDistance >= 2 && PreventBlockSetFlag == false)
+        if (Setup.Player.FallDistance >= 2 && preventBlockSetFlag == false)
         {
             // 更新したら前のインスタンスはGCで開放される
-            PreventBlockPosition = new Position(Setup.player.LatestPosition.X, Setup.player.LatestPosition.Y - 2, Setup.player.LatestPosition.Z);
+            preventBlockPosition = new Position(Setup.Player.LatestPosition.X, Setup.Player.LatestPosition.Y - 2, Setup.Player.LatestPosition.Z);
             //配置場所がairかどうか確認
-            string result = Setup.command.SendCommand($"execute if block {PreventBlockPosition.X} {PreventBlockPosition.Y} {PreventBlockPosition.Z} air");
-            if (result == "Test passed")
+            string _result = Setup.Command.SendCommand($"execute if block {preventBlockPosition.X} {preventBlockPosition.Y} {preventBlockPosition.Z} air");
+            if (_result == "Test passed")
             {
-                Setup.command.SendCommand($"fill {PreventBlockPosition.X - 1} {PreventBlockPosition.Y} {PreventBlockPosition.Z - 1} {PreventBlockPosition.X + 1} {PreventBlockPosition.Y} {PreventBlockPosition.Z + 1} minecraft:barrier");
-                Setup.command.SendCommand($"fill {PreventBlockPosition.X - 1} {PreventBlockPosition.Y + 1} {PreventBlockPosition.Z - 1} {PreventBlockPosition.X + 1} {PreventBlockPosition.Y + 1} {PreventBlockPosition.Z + 1} minecraft:{UseBlockName}");
-                PreventBlockSetFlag = true;
+                Setup.Command.SendCommand($"fill {preventBlockPosition.X - 1} {preventBlockPosition.Y} {preventBlockPosition.Z - 1} {preventBlockPosition.X + 1} {preventBlockPosition.Y} {preventBlockPosition.Z + 1} minecraft:barrier");
+                Setup.Command.SendCommand($"fill {preventBlockPosition.X - 1} {preventBlockPosition.Y + 1} {preventBlockPosition.Z - 1} {preventBlockPosition.X + 1} {preventBlockPosition.Y + 1} {preventBlockPosition.Z + 1} minecraft:{useBlockName}");
+                preventBlockSetFlag = true;
             }
         }
-        else if (Setup.player.FallDistance == 0 && PreventBlockSetFlag == true)
+        else if (Setup.Player.FallDistance == 0 && preventBlockSetFlag == true)
         {
-            Setup.command.SendCommand($"fill {PreventBlockPosition.X - 1} {PreventBlockPosition.Y} {PreventBlockPosition.Z - 1} {PreventBlockPosition.X + 1} {PreventBlockPosition.Y + 1} {PreventBlockPosition.Z + 1} minecraft:air[] destroy");
-            PreventBlockSetFlag = false;
+            Setup.Command.SendCommand($"fill {preventBlockPosition.X - 1} {preventBlockPosition.Y} {preventBlockPosition.Z - 1} {preventBlockPosition.X + 1} {preventBlockPosition.Y + 1} {preventBlockPosition.Z + 1} minecraft:air[] destroy");
+            preventBlockSetFlag = false;
         }
     }
 }
